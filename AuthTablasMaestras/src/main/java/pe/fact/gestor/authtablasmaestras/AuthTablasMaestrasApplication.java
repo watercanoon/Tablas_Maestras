90544e1a13c3865349c9b9c8527e77a0fa22c81b
watercannon <0333241024@unjfsc.edu.pe>
2026-01-12 18:39:03 -0500

    Refactor: Arquitectura modular por dominios (Cliente, Plan, Producto, Tecnico, Concepto, Personal)

---
+++ AuthTablasMaestras/src/main/java/pe/fact/gestor/authtablasmaestras/AuthTablasMaestrasApplication.java
@@ -0,0 +1,13 @@
+package pe.fact.gestor.authtablasmaestras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTablasMaestrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthTablasMaestrasApplication.class, args);
+    }
+
+}
---
+++ AuthTablasMaestras/src/test/java/pe/fact/gestor/authtablasmaestras/AuthTablasMaestrasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.fact.gestor.authtablasmaestras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthTablasMaestrasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
